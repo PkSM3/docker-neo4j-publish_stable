8739c91f9741ba6aaaf7ee30418e29a32770c50a
PkSM3 <pokesam3@gmail.com>
2018-04-17 09:45:53 +0200

    index node keys

--- 3.3.4/community/docker-entrypoint.sh
+++ 3.3.4/community/docker-entrypoint.sh
@@ -89,8 +89,8 @@ fi
 # Set some to default values if unset
 : ${NEO4J_dbms_tx__log_rotation_retention__policy:=${NEO4J_dbms_txLog_rotation_retentionPolicy:-"100M size"}}
 : ${NEO4J_wrapper_java_additional:=${NEO4J_UDC_SOURCE:-"-Dneo4j.ext.udc.source=docker"}}
-: ${NEO4J_dbms_memory_heap_initial__size:=${NEO4J_dbms_memory_heap_maxSize:-"512M"}}
-: ${NEO4J_dbms_memory_heap_max__size:=${NEO4J_dbms_memory_heap_maxSize:-"512M"}}
+: ${NEO4J_dbms_memory_heap_initial__size:=${NEO4J_dbms_memory_heap_maxSize:-"1g"}}
+: ${NEO4J_dbms_memory_heap_max__size:=${NEO4J_dbms_memory_heap_maxSize:-"4g"}}
 : ${NEO4J_dbms_unmanaged__extension__classes:=${NEO4J_dbms_unmanagedExtensionClasses:-}}
 : ${NEO4J_dbms_allow__format__migration:=${NEO4J_dbms_allowFormatMigration:-}}
 : ${NEO4J_dbms_connectors_default__advertised__address:=${NEO4J_dbms_connectors_defaultAdvertisedAddress:-}}
@@ -118,10 +118,13 @@ fi
 : ${NEO4J_dbms_udc_enabled:="false"}
 : ${NEO4J_browser_remote__content__hostname__whitelist:="*"}
 : ${NEO4J_dbms_allow__format__migration:="true"}
+
 : ${NEO4J_dbms_auto__index_nodes_enabled:="true"}
+: ${NEO4J_dbms_auto__index_nodes_keys:="name,country,city,region"}
+
 : ${NEO4J_dbms_memory_heap_initial__size:=1g}
-: ${NEO4J_dbms_memory_heap_max__size:=6g}
-: ${NEO4J_dbms_memory_pagecache_size:="5g"}
+: ${NEO4J_dbms_memory_heap_max__size:=4g}
+: ${NEO4J_dbms_memory_pagecache_size:="4g"}
 
 
 # unset old hardcoded unsupported env variables
@@ -142,8 +145,8 @@ unset NEO4J_dbms_txLog_rotation_retentionPolicy NEO4J_UDC_SOURCE \
 : ${NEO4J_dbms_tx__log_rotation_retention__policy:=100M size}
 : ${NEO4J_dbms_memory_pagecache_size:=512M}
 : ${NEO4J_wrapper_java_additional:=-Dneo4j.ext.udc.source=docker}
-: ${NEO4J_dbms_memory_heap_initial__size:=512M}
-: ${NEO4J_dbms_memory_heap_max__size:=512M}
+: ${NEO4J_dbms_memory_heap_initial__size:=1g}
+: ${NEO4J_dbms_memory_heap_max__size:=4g}
 : ${NEO4J_dbms_connectors_default__listen__address:=0.0.0.0}
 : ${NEO4J_dbms_connector_http_listen__address:=0.0.0.0:7474}
 : ${NEO4J_dbms_connector_https_listen__address:=0.0.0.0:7473}
